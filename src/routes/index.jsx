422cbadc2d79d1dd3e5d6215e392eea67c38b8e9
amir-mansoor <amirmansoor200@gmail.com>
2023-11-08 19:27:35 +0500

    index js

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+        This is a demo for React Router.
+        <br />
+        Check out{" "}
+        <a target="_blank" href="https://github.com/amir-mansoor/react-router-dom">
+          The Code Of The App.
+        </a>
+        .
+      </p>
+    );
+  }
+  
